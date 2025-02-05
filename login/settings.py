1171972f292f0b61045d4509e459e19a3e0164a7
obikiel <ezekielobiomachi1@gmail.com>
2025-02-06 00:00:44 +0100

    allowed host

--- login/settings.py
+++ login/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7h=-7%9zdc&%jicyv25wkx7h&sr6f+=&v@*gq4r@a&_8qu^!)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hng-stage0-ulwt.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
